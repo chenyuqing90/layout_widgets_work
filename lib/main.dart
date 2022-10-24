1e74ed2c51060a016646b7602fca316feec86c39
chenyuqing90 <408630399@gms.tku.edu.tw>
2022-10-24 19:10:30 +0800

    layout widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var appTitle = Text('我的第一個Flutter App'),
+  var appTitle = Text('Flutter App',),
       hiFlutter = Text(
         'hi,Flutter.\n今日作業0926',
         style: TextStyle(fontSize: 30,
@@ -11,18 +11,45 @@ void main() {
         textAlign: TextAlign.justify,
       );
 
-  var appBody = Column(
-    children: const <Widget>[
-      Text('第一行文字', style: TextStyle(fontSize: 30),),
-      Text('第二行文字', style: TextStyle(fontSize: 30),),
-      Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain,
-            child: FlutterLogo(),
-          )),
-]
+  var appBody = GridView.count(
+    primary: false,
+    padding: const EdgeInsets.all(20),
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 10,
+    crossAxisCount: 2,
+    children: <Widget>[
+      Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors.teal[100],
+        child: const Text("He'd have you all unravel at the"),
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors.teal[200],
+        child: const Text('Heed not the rabble'),
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors.teal[300],
+        child: const Text('Sound of screams but the'),
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors.teal[400],
+        child: const Text('Who scream'),
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors.teal[500],
+        child: const Text('Revolution is coming...'),
+      ),
+      Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors.teal[600],
+        child: const Text('Revolution, they...'),
+      ),
+    ],
   );
-
 
   var appBar = AppBar(
     title: appTitle,
